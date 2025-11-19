83a140a77828c14e262be024023a8dc8632d3242
Cheryl Clarke <cherylclark90@live.co.uk>
2025-11-19 17:39:44 +0000

    practice

--- my work/readfile.py
+++
@@ -1,7 +0,0 @@
-FILENAME="numbers.txt"
-DATADIR= ". .\\my work 2\\"
-FILEPATH = "FILENAME + DATADIR"
-
-with open(FILEPATH, "rt") as fP:
-    for line in fP:
-        print(line)
---
+++ practice/readfile.py
@@ -0,0 +1,10 @@
+# program that reads a file and takes action
+# Author : Andrew Beatty
+
+FILENAME="numbers.txt"
+# IF FILE IS IN DIFFERENT DIRECTORY, SET PATH HERE
+DATADIR="../../DATA/"
+
+FULLPATH = DATADIR + FILENAME
+
+print (FULLPATH)
