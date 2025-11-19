18ed634a19a9b60392f32a7bd989ce162d5bb21d
Cheryl Clarke <cherylclark90@live.co.uk>
2025-11-19 19:32:39 +0000

    refresh

--- practice/readfile.py
+++ practice/readfile.py
@@ -1,14 +1,23 @@
 # program that reads a file and takes action
 # Author : Andrew Beatty
 
-FILENAME="numbers.txt"
+FILENAME="number.txt"
 # IF FILE IS IN DIFFERENT DIRECTORY, SET PATH HERE
-DATADIR= "../../my work/"
+DATADIR= "../my work/"
 
 FULLPATH = DATADIR + FILENAME
 
 # print (FULLPATH)
 
 with open (FULLPATH, "rt") as fp:
+    total = 0
     for line in fp:
-        print (line)
+
+      #  print (f" {line} has length {len(line)}")
+
+        print (f" {line.strip()} ", end="")
+        print ( f"has length {len(line)}")
+        total += int(line)
+    print (total)
+# this will put them ob the same line to avoid last line break (new line characters)
+   
