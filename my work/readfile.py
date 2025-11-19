f444d0a7bbafbf703dce4656aaeb4eda9b48d113
Cheryl Clarke <cherylclark90@live.co.uk>
2025-11-19 17:09:11 +0000

    assignment 1 Q

--- my work/readfile.py
+++ my work/readfile.py
@@ -1,6 +1,6 @@
 FILENAME="numbers.txt"
-DATADIR="..\\my work 2\\"
-FILEPATH = "DATADIR + FILENAME"
+DATADIR= ". .\\my work 2\\"
+FILEPATH = "FILENAME + DATADIR"
 
 with open(FILEPATH, "rt") as fP:
     for line in fP:
