5f537d35dfcff2990d88b0f94639577835d26b1d
Cheryl Clarke <cherylclark90@live.co.uk>
2025-11-30 22:55:29 +0000

    population analysis

--- code/populationbyage.py
+++ code/populationbyage.py
@@ -6,5 +6,22 @@ FULLPATH= DATADIR + FILENAME
 
 df = pd.read_csv(FULLPATH)
 
+
+drop_col_list = ["Statistic Label","CensusYear","Sex","UNIT"]
+
+# can be done like this-
+# df =df.drop(columns=drop_col_list)
+# or like this-
+df.drop(columns=drop_col_list, inplace=True)
+
+# to change from all ages to single year of age
+
+df = df[df["Single Year of Age"] != "All ages"]
+
+# changing the column, replace under 1 with 0
+df["Single Year of Age"] = df["Single Year of Age"].str.replace("Under 1 year",'0')
+
+
+
 print (df.head(3))
 
