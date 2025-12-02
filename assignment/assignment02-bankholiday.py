71ef865924e506c76c63e917e486e0a34d0d6885
Cheryl Clarke <cherylclark90@live.co.uk>
2025-12-02 23:23:53 +0000

    part 2

--- assignment/assignment02-bankholiday.py
+++ assignment/assignment02-bankholiday.py
@@ -1,6 +1,15 @@
+# breakdown and referencing in assignment.ipynb file
+
+# part 1.
 import requests
 
 url = "https://www.gov.uk/bank-holidays.json"
 response = requests.get(url)
 data = response.json()
 print(data)
+
+for event in data["northern-ireland"]["events"]:
+    print(f"{event["title"]} on {event['date']}")
+
+# part 2.
+
